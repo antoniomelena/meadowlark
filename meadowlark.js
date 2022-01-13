b7125d7e839a08f2787d3ba5353315df14289bda
Antonio Melena <melenaantonio@gmail.com>
2022-01-13 13:39:46 -0600

    added template with nursery rhyme

--- meadowlark.js
+++ meadowlark.js
@@ -7,6 +7,13 @@ var app = express();
 // set up handlebars view engine
 var handlebars = require("express3-handlebars").create({
   defaultLayout: "main",
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    },
+  },
 });
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
@@ -18,6 +25,40 @@ app.use(express.static(__dirname + "/public"));
 app.use(function (req, res, next) {
   res.locals.showTests =
     app.get("env") !== "production" && req.query.test === "1";
+  next();
+});
+
+function getWeatherData() {
+  return {
+    locations: [
+      {
+        name: "Portland",
+        forecastUrl: "http://www.wunderground.com/US/OR/Portland.html",
+        iconUrl: "http://icons-ak.wxug.com/i/c/k/cloudy.gif",
+        weather: "Overcast",
+        temp: "54.1 F (12.3 C)",
+      },
+      {
+        name: "Bend",
+        forecastUrl: "http://www.wunderground.com/US/OR/Bend.html",
+        iconUrl: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
+        weather: "Partly Cloudy",
+        temp: "55.0 F (12.8 C)",
+      },
+      {
+        name: "Manzanita",
+        forecastUrl: "http://www.wunderground.com/US/OR/Manzanita.html",
+        iconUrl: "http://icons-ak.wxug.com/i/c/k/rain.gif",
+        weather: "Light Rain",
+        temp: "55.0 F (12.8 C)",
+      },
+    ],
+  };
+}
+
+app.use(function (req, res, next) {
+  if (!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weather = getWeatherData();
   next();
 });
 
@@ -42,6 +83,19 @@ app.get("/tours/oregon-coast", function (req, res) {
 
 app.get("/tours/request-group-rate", function (req, res) {
   res.render("tours/request-group-rate");
+});
+
+app.get("/nursery-rhyme", function (req, res) {
+  res.render("nursery-rhyme");
+});
+
+app.get("/data/nursery-rhyme", function (req, res) {
+  res.json({
+    animal: "squirrel",
+    bodyPart: "tail",
+    adjective: "bushy",
+    noun: "heck",
+  });
 });
 
 // custom 404 page
